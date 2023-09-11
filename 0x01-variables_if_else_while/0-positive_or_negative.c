11a7897ff8245a925166317a1bc45e7f9ce24372
fawazthefirst <abdulazizfawaz18@gmail.com>
2023-09-11 06:33:35 -0700

    0. Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,13 @@
 #include <time.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+* main - Determines if a number is positive, negative or zero.
+*
+* Return: Always 0 (Success)
+*/
+
 int main(void)
-/*This is a program to print a number using conditional statements*/
 {
 int n;
 
